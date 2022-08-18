6af78b2fef548da36e76267d5be8b2f354e8f487
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2022-08-18 18:33:29 -0500

    Added filters files fixes

--- filters/join.js
+++ filters/join.js
@@ -1,5 +1,3 @@
-const twig = require('twig')
-
-module.exports = function () {
-    return Reflect.apply(twig.filters.join, null, arguments)
+module.exports = function (array) {
+    return array.join();
 }
